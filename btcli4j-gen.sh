ff9ad66aff768687eb13ad20e44681b09943b5a9
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-11-07 22:42:08 +0100

    Update the rpc version.
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- btcli4j-gen.sh
+++ btcli4j-gen.sh
@@ -1,3 +1,4 @@
-# Script generated from gradle! By clightning4j
 #!/bin/bash
-/home/vincent/.jdks/adopt-openjdk-14.0.2/bin/java -jar /home/vincent/Github/btcli4j/build/libs/btcli4j-all.jar
+# Script generated from gradle! By clightning4j
+
+/usr/lib/jvm/jdk-11.0.10/bin/java -jar /home/vincent/Github/clightning4j/btcli4j/build/libs/btcli4j-all.jar
