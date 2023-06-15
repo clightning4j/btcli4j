90f355587a23be8cd9afc65486d55888fb7334df
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-06-15 12:54:13 +0200

    feat: update the cln lib to 2.4
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- btcli4j-gen.sh
+++ btcli4j-gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script generated from gradle! By clightning4j
 
-/usr/lib/jvm/java-19-openjdk/bin/java -jar /home/vincent/Github/clightning4j/btcli4j/build/libs/btcli4j-all.jar
+/usr/lib/jvm/java-20-openjdk/bin/java -jar /home/vincent/Github/clightning4j/btcli4j/build/libs/btcli4j-all.jar
